ad05ac3112cfedcc7e55b95bf9abcb0574a6f132
Secure Collaboration <secure.collaboration@theguardian.com>
2025-02-04 09:55:56 +0000

    Project import generated by Copybara.
    GitOrigin-RevId: be2abe72f8100027cf3d2d0708cb1a9af7980e34

--- Sources/CoverDropUserInterface/OnboardingView.swift
+++ Sources/CoverDropUserInterface/OnboardingView.swift
@@ -3,15 +3,15 @@ import SwiftUI
 
 struct OnboardingView: View {
     @ObservedObject var navigation = Navigation.shared
-    @State var isNewPassphraseModalOpen = false
+    @ObservedObject var viewModel = OnboardingViewModel()
 
     var body: some View {
         HeaderView(type: .onboarding, dismissAction: {
             navigation.destination = .home
         }) {
             VStack(alignment: .leading, spacing: 0) {
-                TabView {
-                    ForEach(OnboardingViewModel.OnboardingSteps.allCases) { step in
+                TabView(selection: $viewModel.currentStep) {
+                    ForEach(OnboardingSteps.allCases) { step in
                         onboardingStep(step: step)
                     }
                 }
@@ -21,8 +21,12 @@ struct OnboardingView: View {
             .foregroundColor(Color.white)
             Spacer()
 
-            Button("Continue") {
-                navigation.destination = .newPassphrase
+            Button(viewModel.currentStep.buttonText) {
+                if viewModel.isFinalStep() {
+                    navigation.destination = .newPassphrase
+                } else {
+                    advanceToNextTab()
+                }
             }
             .buttonStyle(PrimaryButtonStyle(isDisabled: false))
             .padding([.bottom, .leading, .trailing], Padding.large)
@@ -30,7 +34,13 @@ struct OnboardingView: View {
         .navigationBarHidden(true)
     }
 
-    func onboardingStep(step: OnboardingViewModel.OnboardingSteps) -> some View {
+    func advanceToNextTab() {
+        withAnimation {
+            viewModel.advanceToNextTab()
+        }
+    }
+
+    func onboardingStep(step: OnboardingSteps) -> some View {
         GeometryReader { metric in
             VStack(alignment: .center, spacing: 0) {
                 Group {
@@ -59,51 +69,79 @@ struct OnboardingView: View {
     }
 }
 
-enum OnboardingViewModel {
-    enum OnboardingSteps: String, CustomStringConvertible, CaseIterable, Identifiable {
-        var id: Self { self }
+class OnboardingViewModel: ObservableObject {
+    @Published var currentStep: OnboardingSteps = .sendMessage
 
-        case getPassphrase = "Memorise your passphrase"
-        case sendMessage = "Send a message"
-        case checkResponse = "Check back for a response"
-
-        var description: String {
-            switch self {
-            case .getPassphrase:
-                return """
-                You will been shown a unique passphrase which you will use to unlock your secure inbox. \
-                Please make sure you memorise it so you can return to your secure inbox later.
-                """
-            case .sendMessage:
-                return """
-                Your message will be sent anonymously and securely to a journalist or a team. \
-                It will be encrypted and indistinguishable from normal network traffic.
-                """
-            case .checkResponse:
-                return """
-                Our journalists monitor secure messages regularly. \
-                If they wish to take your story further they will respond here. \
-                To stay in touch you will need to return to your secure inbox using your passphrase.
-                """
-            }
+    func advanceToNextTab() {
+        switch currentStep {
+        case .sendMessage:
+            currentStep = .checkResponse
+        case .checkResponse:
+            currentStep = .getPassphrase
+        case .getPassphrase:
+            break
         }
+    }
 
-        var image: SVGView? {
-            var imageUrl: String
+    func isFinalStep() -> Bool {
+        currentStep == .getPassphrase
+    }
+}
 
-            switch self {
-            case .getPassphrase:
-                imageUrl = "memorisePassphraseIcon"
-            case .sendMessage:
-                imageUrl = "sendAMessageIcon"
-            case .checkResponse:
-                imageUrl = "checkBackForResponseIcon"
-            }
-            if let svg = Bundle.module.url(forResource: imageUrl, withExtension: "svg") {
-                return SVGView(contentsOf: svg)
-            } else {
-                return nil
-            }
+enum OnboardingSteps: String, CustomStringConvertible, CaseIterable, Identifiable {
+    var id: Self { self }
+
+    case sendMessage = "Send a message"
+    case checkResponse = "Check back for a response"
+    case getPassphrase = "Memorise your passphrase"
+
+    var description: String {
+        switch self {
+        case .sendMessage:
+            return """
+            Your message will be sent anonymously and securely to a journalist or a team. \
+            It will be encrypted and indistinguishable from normal network traffic.
+            """
+        case .checkResponse:
+            return """
+            Our journalists monitor secure messages regularly. \
+            If they wish to take your story further they will respond here. \
+            To stay in touch you will need to return to your secure inbox using your passphrase.
+            """
+        case .getPassphrase:
+            return """
+            You will been shown a unique passphrase which you will use to unlock your secure inbox. \
+            Please make sure you memorise it so you can return to your secure inbox later.
+            """
+        }
+    }
+
+    var image: SVGView? {
+        var imageUrl: String
+
+        switch self {
+        case .sendMessage:
+            imageUrl = "sendAMessageIcon"
+        case .checkResponse:
+            imageUrl = "checkBackForResponseIcon"
+        case .getPassphrase:
+            imageUrl = "memorisePassphraseIcon"
+        }
+        if let svg = Bundle.module.url(forResource: imageUrl, withExtension: "svg") {
+            return SVGView(contentsOf: svg)
+        } else {
+            return nil
+        }
+    }
+
+    var buttonText: String {
+        switch self {
+        case .sendMessage:
+            return "Continue"
+        case .checkResponse:
+            return "Continue"
+        case .getPassphrase:
+            return "Set up my passphrase"
         }
     }
 }
